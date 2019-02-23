3d3aeb0ded46cc45a401f7c193b7ff873537b996
boyshu <admin@boyhu.cn>
2019-02-23 22:23:50 +0800

    第一次上传

---
+++ usr/themes/armx/about.php
@@ -0,0 +1,26 @@
+<?php 
+/**
+* 关于
+*
+* @package custom
+*/
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+$this->need('header.php');
+?>
+
+<div id="main" class="main">
+  <h3 class="box-label"><span class="label-left"></span><span class="box-name" id="box-name">
+    <?php $this->title() ?>
+    </span></h3>
+  <article class="article" id="article">
+    <div class="article-box card">
+      <div class="article-content singlepage">
+        <?php parseContent($this,1); ?>
+      </div>
+    </div>
+  </article>
+  <?php $this->need('comments.php'); ?>
+</div>
+<?php $this->need('sidebar.php'); ?>
+<div class="template-links"></div>
+<?php $this->need('footer.php'); ?>
